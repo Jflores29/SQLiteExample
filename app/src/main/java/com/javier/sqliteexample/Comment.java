c7eccb8154128a361083775193f90383bb6e490a
Javier <jflores71814@yahoo.com>
2017-02-17 11:38:56 -0500

    first commit

---
+++ app/src/main/java/com/javier/sqliteexample/Comment.java
@@ -0,0 +1,32 @@
+package com.javier.sqliteexample;
+
+/**
+ * Created by User on 2/16/2017.
+ */
+
+public class Comment {
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString(){
+        return comment;
+    }
+
+}
---
+++ app/src/main/java/com/javier/sqliteexample/TestDatabaseActivity.java
@@ -0,0 +1,75 @@
+package com.javier.sqliteexample;
+
+import android.app.ListActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+import java.util.Random;
+
+public class TestDatabaseActivity extends ListActivity {
+
+    private  CommentDataSource dataSource;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_database);
+
+        dataSource = new CommentDataSource(this);
+        dataSource.open();
+
+        List<Comment> values = dataSource.getAllComments();
+
+
+        ArrayAdapter<Comment> adapter = new ArrayAdapter<Comment>(this,
+                android.R.layout.simple_list_item_1, values);
+
+        setListAdapter(adapter);
+
+    }
+
+
+    // Will be called via the onClick attribute
+    // of the buttons in main.xml
+    public void onClick(View view) {
+        @SuppressWarnings("unchecked")
+        ArrayAdapter<Comment> adapter = (ArrayAdapter<Comment>) getListAdapter();
+        Comment comment = null;
+        switch (view.getId()) {
+            case R.id.add:
+                String[] comments = new String[] { "Cool", "Very nice", "Hate it" };
+                int nextInt = new Random().nextInt(3);
+                // save the new comment to the database
+                comment = dataSource.createComment(comments[nextInt]);
+                adapter.add(comment);
+                break;
+            case R.id.delete:
+                if (getListAdapter().getCount() > 0) {
+                    comment = (Comment) getListAdapter().getItem(0);
+                    dataSource.deleteComment(comment);
+                    adapter.remove(comment);
+                }
+                break;
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onResume() {
+        dataSource.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        dataSource.close();
+        super.onPause();
+    }
+
+
+
+}
